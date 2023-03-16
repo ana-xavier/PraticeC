3672ed6d54f0edd13f07cee587ad2afe10f56339
Ana Xavier <78264318+ana-xavier@users.noreply.github.com>
2023-03-16 11:01:04 -0300

    Encontre o valor no vetor (att)

--- StartingWithPointers/procValor.c
+++ StartingWithPointers/procValor.c
@@ -13,20 +13,21 @@ int main() {
     
     int pos = procurar_valor(vetor, TAMVET, valor);
     
-    if(pos == 1)
+    if(pos == -1)
         printf("Valor %d não foi encontrado na lista\n", valor);
     else
-        printf("O valor %d está na posição %d do vetor\n", valor);
+        printf("O valor %d está na posição %d do vetor\n", valor, pos);
 }
 
 int procurar_valor(int *vetor, int tamanho, int valor) {
     printf("Valor buscado: %d\n", valor);
-    printf("Endereço do vetor: %p\n", $vetor);
+    printf("Endereço do vetor: %p\n", vetor);
     printf("Tamanho do vetor: %d\n", tamanho);         //tamanho de um inteiro (4 bytes)
     
-    for (int i = 0; i < tamanho; i++) 
+    for (int i = 0; i < tamanho; i++) {
         if (*vetor == valor)           //o que há naquele endereço? é igual ao valor buscado?
             return i;
         vetor++;                       //vetor++ vai para o próximo endereço.
+    }    
     return -1;
 }
